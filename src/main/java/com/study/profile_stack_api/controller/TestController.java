e55d43ea4f01796f4f5451197aa15396aaba9d94
siho <dolphin@kakao.com>
2026-02-17 14:34:53 +0900

    feat: HTTP 응답을 감싸는 클래스 ApiResponse 추가

--- src/main/java/com/study/profile_stack_api/controller/TestController.java
+++ src/main/java/com/study/profile_stack_api/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.study.profile_stack_api.controller;
 
+import com.study.profile_stack_api.global.common.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
     @GetMapping
-    public String test() {
-        return "test";
+    public ResponseEntity<ApiResponse<String>> test() {
+        return ResponseEntity.ok().body(ApiResponse.success("Hello World"));
     }
 }
